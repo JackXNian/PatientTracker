db7cf8f55f00d5e334ecc6e7d30840b4cf80f45e
Xin <jackxnian@gmail.com>
2021-10-11 08:42:32 +0200

    start on creating appointment form

---
+++ app/src/main/java/com/example/patienttracker/Activity_Patient_ViewAppointment.java
@@ -0,0 +1,14 @@
+package com.example.patienttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Patient_ViewAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_view_appointment);
+    }
+}
