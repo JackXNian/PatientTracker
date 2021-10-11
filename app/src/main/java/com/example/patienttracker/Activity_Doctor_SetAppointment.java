8e21af4d792f6cd1914e8372fc766896533d01d2
Xin <jackxnian@gmail.com>
2021-10-11 07:34:21 +0200

    add new activity for doctor to set the details of the appointment

---
+++ app/src/main/java/com/example/patienttracker/Activity_Doctor_SetAppointment.java
@@ -0,0 +1,14 @@
+package com.example.patienttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Doctor_SetAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_set_appointment);
+    }
+}
