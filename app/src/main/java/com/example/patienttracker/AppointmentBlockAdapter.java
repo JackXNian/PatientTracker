d41795b9229c47ba7a2dbe0b19495eb30f9f6a0e
Xin <jackxnian@gmail.com>
2021-10-11 06:07:30 +0200

    idk

--- app/src/main/java/com/example/patienttracker/AppointmentBlockAdapter.java
+++ app/src/main/java/com/example/patienttracker/AppointmentBlockAdapter.java
@@ -3,7 +3,9 @@ package com.example.patienttracker;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,11 +19,20 @@ public class AppointmentBlockAdapter extends RecyclerView.Adapter<AppointmentBlo
         public TextView tvAppDate;
         public TextView tvDocName;
         public TextView tvDocNum;
+        public Button btn_view;
         public AppointmentViewHolder(@NonNull View itemView) {
             super(itemView);
-                tvAppDate = itemView.findViewById(R.id.TV_AppDate);
-                tvDocName = itemView.findViewById(R.id.TV_DoctorName);
-                tvDocNum = itemView.findViewById(R.id.TV_DoctorNumber);
+                tvAppDate = itemView.findViewById(R.id.TV_C_Appointment_AppDate);
+                tvDocName = itemView.findViewById(R.id.TV_C_Appointment_DoctorName);
+                tvDocNum = itemView.findViewById(R.id.TV_C_Appointment_DoctorNumber);
+                btn_view = itemView.findViewById(R.id.B_C_Appointment_View);
+
+                btn_view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(v.getContext(), "Hello Java point",Toast.LENGTH_SHORT).show();
+                    }
+                });
         }
     }
 
@@ -33,8 +44,8 @@ public class AppointmentBlockAdapter extends RecyclerView.Adapter<AppointmentBlo
     @Override
     public AppointmentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.appointment_block,parent,false);
-        AppointmentViewHolder avh = new AppointmentViewHolder(v);
-        return avh;
+//        AppointmentViewHolder avh = new AppointmentViewHolder(v);
+        return new AppointmentViewHolder(v);
     }
 
     @Override
@@ -43,7 +54,6 @@ public class AppointmentBlockAdapter extends RecyclerView.Adapter<AppointmentBlo
         holder.tvAppDate.setText(current.getAppointmentDate());
         holder.tvDocName.setText(current.getDoctorName());
         holder.tvDocNum.setText(current.getDoctorNum());
-
 
     }
 
